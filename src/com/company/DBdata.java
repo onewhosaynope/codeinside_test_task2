1ac4224e1e7d91d2c0e73e9da7a0e18175fedbad
Vsevolod <onewhosaynope@gmail.com>
2019-04-28 12:31:16 +0300

    test task #2

--- src/com/company/DBdata.java
+++ src/com/company/DBdata.java
@@ -3,8 +3,8 @@ package com.company;
 public interface DBdata {
 
     public static final String HOST = "jdbc:postgresql://localhost:5432/";
-    public static final String DB_NAME = "codeinside";
+    public static final String DB_NAME = "";
     public static final String USERNAME = "postgres";
-    public static final String PASSWORD = "Sevapazov29";
+    public static final String PASSWORD = "";
 
 }
