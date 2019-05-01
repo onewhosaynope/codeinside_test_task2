aca660384985778c895b088833951d320e44bd91
Сева же, ну <36499717+onewhosaynope@users.noreply.github.com>
2019-05-01 16:42:12 +0300

    Update DBdata.java
    added password

--- src/com/company/DBdata.java
+++ src/com/company/DBdata.java
@@ -5,6 +5,6 @@ public interface DBdata {
     public static final String HOST = "jdbc:postgresql://localhost:5432/";
     public static final String DB_NAME = "codeinside";
     public static final String USERNAME = "postgres";
-    public static final String PASSWORD = "";
+    public static final String PASSWORD = "Sevapazov29";
 
 }
