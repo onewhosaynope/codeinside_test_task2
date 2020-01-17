ce10c9c28dea8fd53491fdbda92cccdea12148db
Сева же, ну <36499717+onewhosaynope@users.noreply.github.com>
2020-01-18 01:35:14 +0300

    Update DBdata.java

--- src/com/company/DBdata.java
+++ src/com/company/DBdata.java
@@ -4,7 +4,7 @@ public interface DBdata {
 
     public static final String HOST = "jdbc:postgresql://localhost:5432/";
     public static final String DB_NAME = "codeinside";
-    public static final String USERNAME = "postgres";
-    public static final String PASSWORD = "Sevapazov29";
+    public static final String USERNAME = "codeinside";
+    public static final String PASSWORD = "codeinside";
 
 }
